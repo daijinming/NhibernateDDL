d282045d1a767febd1eb3ac5456f72db0d6270ce
daijinming <daijinming@live.cn>
2016-12-27 10:27:11 +0800

    Nhibernate自动创建表
    Nhibernate自动创建表

---
+++ NhibernateDDL/Domain/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhibernateDDL.Domain
+{
+    public class Product
+    {   
+        public virtual Guid Id { get; set; }
+        public virtual string Name2 { get; set; }
+        public virtual string Category { get; set; }
+        public virtual bool Discontinued { get; set; }
+    }
+}
---
+++ NhibernateDDL/NHibernateHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NHibernate;
+using NHibernate.Cfg;
+using NhibernateDDL.Domain;
+
+namespace NhibernateDDL
+{
+    public class NHibernateHelper
+    {
+        private ISessionFactory _sessionFactory;
+        public NHibernateHelper()
+        {
+            _sessionFactory = GetSessionFactory();
+        }
+
+        private ISessionFactory GetSessionFactory()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            Assembly thisAssembly = typeof(Product).Assembly;
+            cfg.AddAssembly(thisAssembly);
+
+            return cfg.BuildSessionFactory();
+        }
+        public ISession GetSession()
+        {
+            return _sessionFactory.OpenSession();
+        }
+    }
+}
